8d550c086b8d3e5b9437efa2c4a99e03832135bd
billiamlumbergh <wertjake@gmail.com>
2015-02-28 16:35:07 -0600

    test commit

---
+++ Bitchat/Security/KeyGen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Bitchat.Security
+{
+    class KeyGen
+    {
+        private String charMap;
+        private bool didGenKeyStroke;
+        private Random r;
+
+        public KeyGen()
+        {
+            charMap = "abcdefghijklmnopqrstuvwxyz1234567890";
+            didGenKeyStroke = false;
+            r = new Random();
+        }
+
+        public void genKeyStroke()
+        {
+            didGenKeyStroke = true;
+            SendKeys.Send("" + this.charMap[r.Next(this.charMap.Length - 1)]);
+        }
+
+        public bool getDidGenKeyStroke()
+        {
+            return didGenKeyStroke;
+        }
+
+        public void setDidGenKeyStroke(bool didGenKeyStroke)
+        {
+            this.didGenKeyStroke = didGenKeyStroke;
+        }
+    }
+}
